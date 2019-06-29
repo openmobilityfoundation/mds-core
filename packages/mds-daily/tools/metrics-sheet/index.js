6e5ae0da58987d15ef2ba288848ac72f25833307
mdurling <michael@ellis-and-associates.com>
2019-06-29 09:03:56 -0400

    Run ESLint/Prettier

--- packages/mds-daily/tools/metrics-sheet/index.js
+++ packages/mds-daily/tools/metrics-sheet/index.js
@@ -27,20 +27,20 @@ const reportProviders = [
   'e714f168-ce56-4b41-81b7-0b6a4bd26128', // Lyft
   'b79f8687-526d-4ae6-80bf-89b4c44dc071', // Wheels
   '70aa475d-1fcd-4504-b69c-2eeb2107f7be', // Spin
-  '3c95765d-4da6-41c6-b61e-1954472ec6c9'  // Sherpa
-  ]
+  '3c95765d-4da6-41c6-b61e-1954472ec6c9' // Sherpa
+]
 const creds = {
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   private_key: process.env.GOOGLE_PRIVATE_KEY.split('\\n').join('\n')
 }
 
-function sum (arr) {
+function sum(arr) {
   return arr.reduce((total, amount) => total + (amount || 0))
 }
 
-//Round percent to two decimals
+// Round percent to two decimals
 function percent(a, total) {
-  return Math.round((total - a) / total * 10000) / 10000 
+  return Math.round(((total - a) / total) * 10000) / 10000
 }
 
 async function appendSheet(rows) {
@@ -48,85 +48,94 @@ async function appendSheet(rows) {
   try {
     await promisify(doc.useServiceAccountAuth)(creds)
     const info = await promisify(doc.getInfo)()
-    console.log(`Loaded doc: ` + info.title + ` by ` + info.author.email)
+    console.log(`Loaded doc: ${info.title} by ${info.author.email}`)
     const sheet = info.worksheets.filter(sheet => sheet.title === 'Metrics Log')[0]
-    console.log(`Metrics sheet: ` + sheet.title + ` ` + sheet.rowCount + `x` + sheet.colCount)
+    console.log(`Metrics sheet: ${sheet.title} ${sheet.rowCount}x${sheet.colCount}`)
     if (sheet.title === 'Metrics Log') {
       const inserted = rows.map(insert_row => promisify(sheet.addRow)(insert_row))
-      console.log('Wrote ' + inserted.length + ' rows.')
+      console.log(`Wrote ${inserted.length} rows.`)
       return await Promise.all(inserted)
-    } else {
-      console.log('Wrong sheet!')
     }
+    console.log('Wrong sheet!')
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
-
-async function getProviderMetrics () {
-  const token_options = { method: 'POST',
-    url: process.env.AUTH0_DOMAIN + '/oauth/token',
+async function getProviderMetrics() {
+  const token_options = {
+    method: 'POST',
+    url: `${process.env.AUTH0_DOMAIN}/oauth/token`,
     headers: { 'content-type': 'application/json' },
-    body: 
-     { grant_type: 'client_credentials',
-       client_id: process.env.CLIENT_ID,
-       client_secret: process.env.CLIENT_SECRET,
-       audience: process.env.AUDIENCE },
-    json: true 
+    body: {
+      grant_type: 'client_credentials',
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      audience: process.env.AUDIENCE
+    },
+    json: true
   }
   const token = await rp(token_options)
   const counts_options = {
-      uri: 'https://api.ladot.io/agency/admin/vehicle_counts',
-      headers: { authorization: 'Bearer ' + token.access_token },
-      json: true
+    uri: 'https://api.ladot.io/agency/admin/vehicle_counts',
+    headers: { authorization: `Bearer ${token.access_token}` },
+    json: true
   }
   const last_options = {
-      uri: 'https://api.ladot.io/agency/admin/last_day_stats_by_provider',
-      headers: { authorization: 'Bearer ' + token.access_token },
-      json: true
+    uri: 'https://api.ladot.io/agency/admin/last_day_stats_by_provider',
+    headers: { authorization: `Bearer ${token.access_token}` },
+    json: true
   }
 
   const counts = await rp(counts_options)
   const last = await rp(last_options)
 
-  const rows = counts.filter(p => reportProviders.includes(p.provider_id)).map(provider => {
-    const dateOptions = { timeZone: 'America/Los_Angeles', day: '2-digit', month: '2-digit', year: 'numeric' }
-    const timeOptions = { timeZone: 'America/Los_Angeles', hour12: false, hour: '2-digit', minute:'2-digit' }
-    const d = new Date
-    let [starts, ends, start_sla, end_sla] = [0, 0, 0, 0]
-    let event_counts = { service_start: 0, provider_drop_off: 0, trip_start: 0, trip_end: 0 } 
-    if (last[provider.provider_id].event_counts_last_24h) {
-      event_counts = last[provider.provider_id].event_counts_last_24h 
-      starts = last[provider.provider_id].event_counts_last_24h.trip_start || 0
-      ends = last[provider.provider_id].event_counts_last_24h.trip_end || 0
-      telems = last[provider.provider_id].telemetry_counts_last_24h || 0
-      telem_sla = telems ? percent(last[provider.provider_id].late_telemetry_counts_last_24h, telems) : 0
-      start_sla = starts ? percent(last[provider.provider_id].late_event_counts_last_24h.trip_start, starts) : 0
-      end_sla = ends ? percent(last[provider.provider_id].late_event_counts_last_24h.trip_end, ends) : 0
-    } 
-    return { 
-      date: d.toLocaleDateString('en-US', dateOptions) + ' ' + d.toLocaleTimeString('en-US', timeOptions),
-      name: provider.provider,
-      registered: provider.count || 0,
-      deployed: sum([provider.status.available, provider.status.unavailable, provider.status.trip, provider.status.reserved]) || 0,
-      validtrips: 'tbd', // Placeholder for next day valid trip analysis
-      trips: last[provider.provider_id].trips_last_24h || 0,
-      servicestart: event_counts.service_start || 0,
-      providerdropoff: event_counts.provider_drop_off || 0,
-      tripstart: starts,
-      tripend: ends,
-      tripenter: last[provider.provider_id].event_counts_last_24h.trip_enter || 0,
-      tripleave: last[provider.provider_id].event_counts_last_24h.trip_leave || 0,
-      telemetry: telems,
-      telemetrysla: telem_sla,
-      tripstartsla: start_sla,
-      tripendsla: end_sla
-    }
-  })
+  const rows = counts
+    .filter(p => reportProviders.includes(p.provider_id))
+    .map(provider => {
+      const dateOptions = { timeZone: 'America/Los_Angeles', day: '2-digit', month: '2-digit', year: 'numeric' }
+      const timeOptions = { timeZone: 'America/Los_Angeles', hour12: false, hour: '2-digit', minute: '2-digit' }
+      const d = new Date()
+      let [starts, ends, start_sla, end_sla] = [0, 0, 0, 0]
+      let event_counts = { service_start: 0, provider_drop_off: 0, trip_start: 0, trip_end: 0 }
+      if (last[provider.provider_id].event_counts_last_24h) {
+        event_counts = last[provider.provider_id].event_counts_last_24h
+        starts = last[provider.provider_id].event_counts_last_24h.trip_start || 0
+        ends = last[provider.provider_id].event_counts_last_24h.trip_end || 0
+        telems = last[provider.provider_id].telemetry_counts_last_24h || 0
+        telem_sla = telems ? percent(last[provider.provider_id].late_telemetry_counts_last_24h, telems) : 0
+        start_sla = starts ? percent(last[provider.provider_id].late_event_counts_last_24h.trip_start, starts) : 0
+        end_sla = ends ? percent(last[provider.provider_id].late_event_counts_last_24h.trip_end, ends) : 0
+      }
+      return {
+        date: `${d.toLocaleDateString('en-US', dateOptions)} ${d.toLocaleTimeString('en-US', timeOptions)}`,
+        name: provider.provider,
+        registered: provider.count || 0,
+        deployed:
+          sum([
+            provider.status.available,
+            provider.status.unavailable,
+            provider.status.trip,
+            provider.status.reserved
+          ]) || 0,
+        validtrips: 'tbd', // Placeholder for next day valid trip analysis
+        trips: last[provider.provider_id].trips_last_24h || 0,
+        servicestart: event_counts.service_start || 0,
+        providerdropoff: event_counts.provider_drop_off || 0,
+        tripstart: starts,
+        tripend: ends,
+        tripenter: last[provider.provider_id].event_counts_last_24h.trip_enter || 0,
+        tripleave: last[provider.provider_id].event_counts_last_24h.trip_leave || 0,
+        telemetry: telems,
+        telemetrysla: telem_sla,
+        tripstartsla: start_sla,
+        tripendsla: end_sla
+      }
+    })
   return rows
 }
 
-exports.handler = (event, context) => getProviderMetrics()
-        .then(rows => appendSheet(rows))
-        .catch((err) => console.error(err))
+exports.handler = (event, context) =>
+  getProviderMetrics()
+    .then(rows => appendSheet(rows))
+    .catch(err => console.error(err))
