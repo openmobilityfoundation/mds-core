26b6929286497d04aa72ccd978466a9ec48e5f34
mdurling <michael@ellis-and-associates.com>
2019-06-30 14:52:26 -0400

    Simplify the authorization handler

--- aws-lambda/api-gateway-authorizer/index.ts
+++ aws-lambda/api-gateway-authorizer/index.ts
@@ -33,44 +33,30 @@ const generatePolicy = (
   context
 })
 
+interface JWT {
+  [claim: string]: string
+}
+
 // Reusable Authorizer function
 export const handler: Handler<{ authorizationToken: string }, CustomAuthorizerResult> = (event, context, callback) => {
-  console.log('event', event)
-  if (!event.authorizationToken) {
-    return callback('Unauthorized')
-  }
-
-  const [scheme, token] = event.authorizationToken.split(' ')
-
-  if (!(scheme.toLowerCase() === 'bearer' && token)) {
-    // no auth token!
-    return callback('Unauthorized')
-  }
-
-  const options = {
-    audience: AUTH0_API_IDENTIFIER,
-    issuer: TOKEN_ISSUER
-  }
-
-  try {
-    verify(token, AUTH0_CLIENT_PUBLIC_KEY.split('\\n').join('\n'), options, (verifyError, decoded) => {
-      if (verifyError || typeof decoded !== 'object') {
-        console.log('verifyError', verifyError)
-        // 401 Unauthorized
-        console.log(`Token invalid. ${verifyError}`)
-        return callback('Unauthorized')
+  if (event.authorizationToken) {
+    const [scheme, token] = event.authorizationToken.split(' ')
+    if (scheme.toLowerCase() === 'bearer' && token) {
+      try {
+        const decoded = verify(token, AUTH0_CLIENT_PUBLIC_KEY.split('\\n').join('\n'), {
+          audience: AUTH0_API_IDENTIFIER,
+          issuer: TOKEN_ISSUER
+        })
+        if (typeof decoded === 'object') {
+          const { sub: principalId, scope, [TOKEN_PROVIDER_ID_CLAIM]: provider_id, email } = decoded as JWT
+          callback(null, generatePolicy(principalId, { provider_id, scope, email }))
+          return
+        }
+      } catch (err) {
+        console.log(`Token Verification Failure. ${err}`, err)
       }
-      // is custom authorizer function
-      console.log('valid from customAuthorizer', decoded)
-
-      const { sub: principalId, scope, [TOKEN_PROVIDER_ID_CLAIM]: provider_id, email } = decoded as {
-        [c: string]: string
-      }
-
-      return callback(null, generatePolicy(principalId, { provider_id, scope, email }))
-    })
-  } catch (err) {
-    console.log('catch error. Invalid token', err)
-    return callback('Unauthorized')
+    }
   }
+  console.log('Authorization Failure', event)
+  callback('Unauthorized')
 }
