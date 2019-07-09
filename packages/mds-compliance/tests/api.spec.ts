5053a6e0a654757a48870654f13276c2c6dd5bad
Charles Neil Goldader <neilgoldader@gmail.com>
2019-07-09 10:16:58 -0700

    [mds-compliance] [mds-utils] Implementation of compliance/count endpoint (#11)
    First pass at compliance/count endpoint

--- packages/mds-compliance/tests/api.spec.ts
+++ packages/mds-compliance/tests/api.spec.ts
@@ -28,7 +28,6 @@ const DEVICE_UUID = 'ec551174-f324-4251-bfed-28d9f3f473fc'
 const CITY_OF_LA = '1f943d59-ccc9-4d91-b6e2-0c5e771cbc49'
 const LA_BEACH = 'ff822e26-a70c-4721-ac32-2f6734beff9b'
 
-/* eslint-reason can't import untyped JS modules without require */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const restrictedAreas = require('../../ladot-service-areas/restricted-areas')
 const veniceSpecialOpsZone = require('../../ladot-service-areas/venice-special-ops-zone')
@@ -1035,6 +1034,66 @@ describe('Tests Compliance API:', () => {
           test.assert(result.body.length === 1)
           test.assert(result.body[0].compliance[0].matches.length === 0)
           test.value(result).hasHeader('content-type', APP_JSON)
+          done(err)
+        })
+    })
+  })
+
+  describe('Tests count endpoint', () => {
+    before(done => {
+      const devices_a: Device[] = makeDevices(15, now())
+      const events_a = makeEventsWithTelemetry(devices_a, now(), CITY_OF_LA, 'trip_start')
+      const telemetry_a: Telemetry[] = devices_a.reduce((acc: Telemetry[], device) => {
+        return [...acc, makeTelemetryInArea(device, now(), CITY_OF_LA, 10)]
+      }, [])
+
+      const devices_b: Device[] = makeDevices(15, now())
+      const events_b = makeEventsWithTelemetry(devices_b, now(), CITY_OF_LA, 'provider_drop_off')
+      const telemetry_b: Telemetry[] = devices_b.reduce((acc: Telemetry[], device) => {
+        return [...acc, makeTelemetryInArea(device, now(), CITY_OF_LA, 10)]
+      }, [])
+
+      request
+        .get('/test/initialize')
+        .set('Authorization', ADMIN_AUTH)
+        .expect(200)
+        .end(() => {
+          // Seed
+          const seedData = {
+            devices: [...devices_a, ...devices_b],
+            events: [...events_a, ...events_b],
+            telemetry: [...telemetry_a, ...telemetry_b]
+          }
+          Promise.all([db.initialize(), cache.initialize()]).then(() => {
+            Promise.all([cache.seed(seedData), db.seed(seedData)]).then(() => {
+              db.writePolicy(COUNT_POLICY_JSON).then(() => {
+                db.writeGeography({ geography_id: GEOGRAPHY_UUID, geography_json: la_city_boundary }).then(() => {
+                  done()
+                })
+              })
+            })
+          })
+        })
+    })
+
+    it('Test count endpoint success', done => {
+      request
+        .get(`/count/47c8c7d4-14b5-43a3-b9a5-a32ecc2fb2c6`)
+        .set('Authorization', ADMIN_AUTH)
+        .expect(200)
+        .end((err, result) => {
+          test.assert(result.body.count === 30)
+          test.value(result).hasHeader('content-type', APP_JSON)
+          done(err)
+        })
+    })
+
+    it('Test count endpoint failure with bad rule_id', done => {
+      request
+        .get(`/count/33ca0ee8-e74b-419d-88d3-aaaf05ac0509`)
+        .set('Authorization', ADMIN_AUTH)
+        .expect(404)
+        .end(err => {
           done(err)
         })
     })
