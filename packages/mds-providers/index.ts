211365cb372d946141d401eb0fd89ae8a18896e6
mdurling <michael@ellis-and-associates.com>
2019-07-09 13:36:07 -0400

    Remove undocumented Blue* provider id

--- packages/mds-providers/index.ts
+++ packages/mds-providers/index.ts
@@ -24,7 +24,6 @@ export const OJO_PROVIDER_ID = '8d293326-8464-4256-8312-617ebcd0efad'
 export const TEST1_PROVIDER_ID = '5f7114d1-4091-46ee-b492-e55875f7de00'
 export const TEST2_PROVIDER_ID = '45f37d69-73ca-4ca6-a461-e7283cffa01a'
 export const TEST3_PROVIDER_ID = 'c8051767-4b14-4794-abc1-85aad48baff1'
-export const BLUE_TEST_PROVIDER_ID = '11111111-2222-4444-8888-999999999999'
 
 const PROVIDER_IDS = [
   JUMP_PROVIDER_ID,
@@ -46,8 +45,7 @@ const PROVIDER_IDS = [
   OJO_PROVIDER_ID,
   TEST1_PROVIDER_ID,
   TEST2_PROVIDER_ID,
-  TEST3_PROVIDER_ID,
-  BLUE_TEST_PROVIDER_ID
+  TEST3_PROVIDER_ID
 ] as const
 
 type PROVIDER_ID = typeof PROVIDER_IDS[number]
@@ -142,9 +140,6 @@ export const providers: Readonly<{ [P in PROVIDER_ID]: Readonly<Provider> }> = O
     url: 'https://www.ojoelectric.com',
     mds_api_url: 'https://api.ojoelectric.com/api/mds',
     gbfs_api_url: 'https://api.ojoelectric.com/api/mds/gbfs.json'
-  }),
-  [BLUE_TEST_PROVIDER_ID]: Object.freeze({
-    provider_name: 'Blue*'
   }),
   [TEST1_PROVIDER_ID]: Object.freeze({
     provider_name: 'Test 1'
