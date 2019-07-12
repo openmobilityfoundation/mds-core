6b35cc3b7e3a23005663490b30f836d10d84ffc1
mdurling <michael@ellis-and-associates.com>
2019-07-11 20:20:26 -0400

    Reformat webpack-config

--- aws-lambda/mds-metrics-sheet/webpack.config.js
+++ aws-lambda/mds-metrics-sheet/webpack.config.js
@@ -4,7 +4,10 @@ const config = require('../webpack.config')
 
 module.exports = (env, argv) => {
   return merge(config(env, argv), {
-    entry: {'metrics-log':'./metrics-log.ts', 'vehicle-counts':'./vehicle-counts.ts'},
+    entry: {
+      'metrics-log':'./metrics-log.ts',
+      'vehicle-counts':'./vehicle-counts.ts'
+    },
     output: {
       path: path.resolve(__dirname, 'dist')    }
   })
