7e830bd95d66d9b34d941f0e6ef17a644be2a54a
Neil Goldader <neilgoldader@gmail.com>
2019-07-16 11:40:18 -0700

    Cleanup

--- packages/mds-db/index.ts
+++ packages/mds-db/index.ts
@@ -384,7 +384,7 @@ async function updateDevice(device_id: UUID, changes: Partial<Device>): Promise<
 }
 
 async function writeEvent(event_param: VehicleEvent) {
-  const device = await readDevice(event_param.device_id)
+  await readDevice(event_param.device_id)
   const client = await getWriteableClient() 
   const telemetry_timestamp = event_param.telemetry ? event_param.telemetry.timestamp : null
   const event = { ...event_param, telemetry_timestamp }
