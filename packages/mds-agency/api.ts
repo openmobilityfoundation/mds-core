103061c975ab78f6e0cdf954a3c822014325561d
Neil Goldader <neilgoldader@gmail.com>
2019-07-16 13:29:32 -0700

    WIP

--- packages/mds-agency/api.ts
+++ packages/mds-agency/api.ts
@@ -337,7 +337,7 @@ function api(app: express.Express): express.Express {
           await log.warn('/event exception cache/stream', err)
         }
       } catch (err) {
-        await log.error(err)
+        await log.error('writeRegisterEvent failure', err)
         throw new Error('writeEvent exception db')
       }
     }
@@ -352,7 +352,11 @@ function api(app: express.Express): express.Express {
         await log.error('failed to write device stream/cache', err)
       }
       await log.info('new', providerName(res.locals.provider_id), 'vehicle added', JSON.stringify(device))
-      await writeRegisterEvent()
+      try {
+        await writeRegisterEvent()
+      } catch (err) {
+        log.error('writeRegisterEvent failure', err)
+      }
       res.status(201).send({ result: 'register device success', recorded, device })
     } catch (err) {
       if (String(err).includes('duplicate')) {
