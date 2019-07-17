d92ee596f121df74f1483f53e276a246e055fb1d
Charles Neil Goldader <avatarneil@gmail.com>
2019-07-17 10:18:12 -0700

    [mds-*] Add promise linter rules, and clean up half of agency (#45)
    * Add promise linter rules, and clean up half of agency
    * Refactor service_areas endpoint to not use .catch with await

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,20 @@ module.exports = {
   env: {
     node: true,
     es6: true,
-    mocha: true,
+    mocha: true
   },
-  plugins: ["import", "prettier", "@typescript-eslint"],
+  plugins: ['import', 'prettier', '@typescript-eslint', 'promise'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   extends: [
-    "airbnb-base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
   ],
   rules: {
     '@typescript-eslint/camelcase': 'off',
@@ -23,14 +23,14 @@ module.exports = {
     '@typescript-eslint/no-object-literal-type-assertion': [
       'error',
       {
-        allowAsParameter: true, // Allow type assertion in call and new expression, default false
-      },
+        allowAsParameter: true // Allow type assertion in call and new expression, default false
+      }
     ],
     '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'array-callback-return': 'off',
     'consistent-return': 'off',
-    'eqeqeq': 'error',
+    eqeqeq: 'error',
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
@@ -39,6 +39,20 @@ module.exports = {
     'no-restricted-syntax': 'off',
     'no-var': 'error',
     'prettier/prettier': 'warn',
-    'radix': 'off'
+    radix: 'off',
+    'promise/always-return': 'warn',
+    'promise/no-return-wrap': 'warn',
+    'promise/param-names': 'warn',
+    'promise/catch-or-return': 'warn',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/avoid-new': 'warn',
+    'promise/no-new-statics': 'warn',
+    'promise/no-return-in-finally': 'warn',
+    'promise/valid-params': 'warn',
+    'promise/prefer-await-to-then': 'warn',
+    'promise/prefer-await-to-callbacks': 'warn'
   }
 }
