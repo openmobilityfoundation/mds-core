4b0ef4cb508eae610f708cc6fe045256249c7792
mdurling <michael@ellis-and-associates.com>
2019-07-24 08:44:04 -0400

    Use standard name for @types

--- types/ladot-service-areas.d.ts
+++
@@ -1,6 +0,0 @@
-import { any } from "@hapi/joi";
-
-declare module 'ladot-service-areas' {
-  export const serviceAreaMap: {[key: string]: any}
-  export function readServiceAreas(provider_id?: string, service_area_id?: string): any
-}
