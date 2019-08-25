48f7e75bb4cf4bd05bdffa8c07b88bf68fd8a2cf
James Todd <james.w.todd@gmail.com>
2019-08-24 23:48:36 -0700

    spike minimal db-init cypress test with validation

--- cypress/integration/dbInit.js
+++ cypress/integration/dbInit.js
@@ -1,5 +1,14 @@
+// fixme: move creds to fixtures
 describe('dbInit', function() {
   it('successfully initializes', function() {
-    cy.request('http://mds-agency:4001/agency/test/initialize')
+    cy.request({
+      url: 'http://mds-agency:4001/agency/test/initialize',
+      auth: {
+        bearer: ".eyJzY29wZSI6ICJhZG1pbjphbGwgdGVzdDphbGwifQo=",
+      },
+    })
+    .then((resp) => {
+      expect(resp.status).to.eq(200)
+    })
   })
 })
