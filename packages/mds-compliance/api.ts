9f08c5682b52d722aaff1d5b652194985bbe4013
Neil Goldader <neilgoldader@gmail.com>
2019-08-27 09:32:09 -0700

    Slight cleanup

--- packages/mds-compliance/api.ts
+++ packages/mds-compliance/api.ts
@@ -122,7 +122,7 @@ function api(app: express.Express): express.Express {
     if (!isUUID(policy_uuid)) {
       res.status(400).send({ err: 'bad_param' })
     } else {
-      const start_date = (parseInt(query_end_date) || now()) - days(365)
+      const start_date = (query_end_date ? parseInt(query_end_date) : now()) - days(365)
       const end_date = query_end_date ? parseInt(query_end_date) : now() + days(365)
       try {
         const all_policies = await db.readPolicies({ start_date, end_date })
