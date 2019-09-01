69121a1fc718c55f3cea0e2cff51d756714e53fe
mdurling <michael@ellis-and-associates.com>
2019-09-01 09:31:53 -0400

    Simplify api authorizer

--- packages/mds-api-authorizer/index.ts
+++ packages/mds-api-authorizer/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import jwtDecode from 'jwt-decode'
+import decode from 'jwt-decode'
 import { UUID } from '@mds-core/mds-types'
 
 export interface ApiAuthorizerClaims {
@@ -18,30 +18,28 @@ export type ApiAuthorizer = (req: express.Request) => ApiAuthorizerClaims | null
 
 export const AuthorizationHeaderApiAuthorizer: ApiAuthorizer = req => {
   if (req.headers && req.headers.authorization) {
-    const decode = ([scheme, token]: string[]): ApiAuthorizerClaims | null => {
-      const decoders: { [scheme: string]: () => ApiAuthorizerClaims } = {
-        bearer: () => {
-          const {
-            sub: principalId,
-            scope,
-            [TOKEN_PROVIDER_ID_CLAIM]: provider_id,
-            [TOKEN_USER_EMAIL_CLAIM]: user_email,
-            email,
-            ...claims
-          } = jwtDecode(token)
-          return { principalId, scope, provider_id, user_email: user_email || email, ...claims }
-        },
-        basic: () => {
-          const [principalId, scope] = Buffer.from(token, 'base64')
-            .toString()
-            .split('|')
-          return { principalId, scope, provider_id: principalId, user_email: null }
-        }
+    const [scheme, token] = req.headers.authorization.split(' ')
+    const decoders: { [scheme: string]: () => ApiAuthorizerClaims } = {
+      bearer: () => {
+        const {
+          sub: principalId,
+          scope,
+          [TOKEN_PROVIDER_ID_CLAIM]: provider_id,
+          [TOKEN_USER_EMAIL_CLAIM]: user_email,
+          email,
+          ...claims
+        } = decode(token)
+        return { principalId, scope, provider_id, user_email: user_email || email, ...claims }
+      },
+      basic: () => {
+        const [principalId, scope] = Buffer.from(token, 'base64')
+          .toString()
+          .split('|')
+        return { principalId, scope, provider_id: principalId, user_email: null }
       }
-      const decoder = decoders[scheme.toLowerCase()]
-      return decoder ? decoder() : null
     }
-    return decode(req.headers.authorization.split(' '))
+    const decoder = decoders[scheme.toLowerCase()]
+    return decoder ? decoder() : null
   }
   return null
 }
