fa08d48a9269666ef3d337ac64e9334ef8145cfb
mdurling <michael@ellis-and-associates.com>
2019-09-19 09:11:09 -0400

    Fix linter warnings

--- packages/mds-provider/legacy.ts
+++ packages/mds-provider/legacy.ts
@@ -7,16 +7,7 @@ import logger from '@mds-core/mds-logger'
 import db from '@mds-core/mds-db'
 import { isUUID, now, seconds, round } from '@mds-core/mds-utils'
 import { ReadEventsResult, StatusChange, Trip } from '@mds-core/mds-db/types'
-import {
-  VehicleEvent,
-  VEHICLE_TYPE,
-  PROPULSION_TYPE,
-  Telemetry,
-  UUID,
-  Device,
-  VEHICLE_EVENTS,
-  Recorded
-} from '@mds-core/mds-types'
+import { VehicleEvent, Telemetry, UUID, Device, VEHICLE_EVENTS, Recorded } from '@mds-core/mds-types'
 import { providerName } from '@mds-core/mds-providers'
 import { Feature, FeatureCollection } from 'geojson'
 import { asStatusChangeEvent } from './utils'
