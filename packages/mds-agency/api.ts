05a6fbac8d3a57c5beec485e9a4160ea88068474
macsj200 <maxjohansen@berkeley.edu>
2019-09-23 12:05:16 -0700

    WIP

--- packages/mds-agency/api.ts
+++ packages/mds-agency/api.ts
@@ -57,6 +57,7 @@ import {
 } from '@mds-core/mds-utils'
 import { AgencyApiRequest, AgencyApiResponse } from '@mds-core/mds-agency/types'
 import { checkScope } from '@mds-core/mds-api-server'
+import { getAllServiceAreas, getServiceAreaById, registerVehicle } from './request-handlers'
 
 function api(app: express.Express): express.Express {
   /**
@@ -136,213 +137,19 @@ function api(app: express.Express): express.Express {
    * Get all service areas
    * See {@link https://github.com/CityOfLosAngeles/mobility-data-specification/tree/dev/agency#service_areas Service Areas}
    */
-  app.get(pathsFor('/service_areas'), async (req: AgencyApiRequest, res: AgencyApiResponse) => {
-    try {
-      const serviceAreas = await areas.readServiceAreas()
-      await log.info('readServiceAreas (all)', serviceAreas.length)
-      return res.status(200).send({
-        service_areas: serviceAreas
-      })
-    } catch (err) {
-      /* istanbul ignore next */
-      await log.error('failed to read service areas', err)
-      return res.status(404).send({
-        result: 'not found'
-      })
-    }
-  })
+  app.get(pathsFor('/service_areas'), getAllServiceAreas)
 
   /**
    * Get a particular service area
    * See {@link https://github.com/CityOfLosAngeles/mobility-data-specification/tree/dev/agency#service_areas Service Areas}
    */
-  app.get(pathsFor('/service_areas/:service_area_id'), async (req: AgencyApiRequest, res: AgencyApiResponse) => {
-    const { service_area_id } = req.params
-
-    if (!isUUID(service_area_id)) {
-      return res.status(400).send({
-        result: `invalid service_area_id ${service_area_id} is not a UUID`
-      })
-    }
-
-    try {
-      const serviceAreas = await areas.readServiceAreas(undefined, service_area_id)
-
-      if (serviceAreas && serviceAreas.length > 0) {
-        await log.info('readServiceAreas (one)')
-        return res.status(200).send({
-          service_areas: serviceAreas
-        })
-      }
-    } catch {
-      return res.status(404).send({
-        result: `${service_area_id} not found`
-      })
-    }
-
-    return res.status(404).send({
-      result: `${service_area_id} not found`
-    })
-  })
-
-  function badDevice(device: Device): Partial<{ error: string; error_description: string }> | boolean {
-    if (!device.device_id) {
-      return {
-        error: 'missing_param',
-        error_description: 'missing device_id'
-      }
-    }
-    if (!isUUID(device.device_id)) {
-      return {
-        error: 'bad_param',
-        error_description: `device_id ${device.device_id} is not a UUID`
-      }
-    }
-    // propulsion is a list
-    if (!Array.isArray(device.propulsion)) {
-      return {
-        error: 'missing_param',
-        error_description: 'missing propulsion types'
-      }
-    }
-    for (const prop of device.propulsion) {
-      if (!isEnum(PROPULSION_TYPES, prop)) {
-        return {
-          error: 'bad_param',
-          error_description: `invalid propulsion type ${prop}`
-        }
-      }
-    }
-    // if (device.year === undefined) {
-    //     return {
-    //         error: 'missing_param',
-    //         error_description: 'missing integer field "year"'
-    //     }
-    // }
-    if (device.year !== null && device.year !== undefined) {
-      if (!Number.isInteger(device.year)) {
-        return {
-          error: 'bad_param',
-          error_description: `invalid device year ${device.year} is not an integer`
-        }
-      }
-      if (device.year < 1980 || device.year > 2020) {
-        return {
-          error: 'bad_param',
-          error_description: `invalid device year ${device.year} is out of range`
-        }
-      }
-    }
-    if (device.type === undefined) {
-      return {
-        error: 'missing_param',
-        error_description: 'missing enum field "type"'
-      }
-    }
-    if (!isEnum(VEHICLE_TYPES, device.type)) {
-      return {
-        error: 'bad_param',
-        error_description: `invalid device type ${device.type}`
-      }
-    }
-    // if (device.mfgr === undefined) {
-    //     return {
-    //         error: 'missing_param',
-    //         error_description: 'missing string field "mfgr"'
-    //     }
-    // }
-    // if (device.model === undefined) {
-    //     return {
-    //         error: 'missing_param',
-    //         error_description: 'missing string field "model"'
-    //     }
-    // }
-    return false
-  }
+  app.get(pathsFor('/service_areas/:service_area_id'), getServiceAreaById)
 
   /**
    * Endpoint to register vehicles
    * See {@link https://github.com/CityOfLosAngeles/mobility-data-specification/tree/dev/agency#vehicle---register Register}
    */
-  app.post(pathsFor('/vehicles'), async (req: AgencyApiRequest, res: AgencyApiResponse) => {
-    const { body } = req
-    const recorded = now()
-    const device: Device = {
-      provider_id: res.locals.provider_id,
-      device_id: body.device_id,
-      vehicle_id: body.vehicle_id,
-      type: body.type,
-      propulsion: body.propulsion,
-      year: parseInt(body.year) || body.year,
-      mfgr: body.mfgr,
-      model: body.model,
-      recorded,
-      status: VEHICLE_STATUSES.removed
-    }
-
-    const failure = badDevice(device)
-    if (failure) {
-      return res.status(400).send(failure)
-    }
-
-    async function writeRegisterEvent() {
-      const event: VehicleEvent = {
-        device_id: device.device_id,
-        provider_id: device.provider_id,
-        event_type: VEHICLE_EVENTS.register,
-        event_type_reason: null,
-        telemetry: null,
-        timestamp: recorded,
-        trip_id: null,
-        recorded,
-        telemetry_timestamp: undefined,
-        service_area_id: null
-      }
-      try {
-        const recorded_event = await db.writeEvent(event)
-        try {
-          // writing to cache and stream is not fatal
-          await Promise.all([cache.writeEvent(recorded_event), stream.writeEvent(recorded_event)])
-        } catch (err) {
-          await log.warn('/event exception cache/stream', err)
-        }
-      } catch (err) {
-        await log.error('writeRegisterEvent failure', err)
-        throw new Error('writeEvent exception db')
-      }
-    }
-
-    // writing to the DB is the crucial part.  other failures should be noted as bugs but tolerated
-    // and fixed later.
-    try {
-      await db.writeDevice(device)
-      try {
-        await Promise.all([cache.writeDevice(device), stream.writeDevice(device)])
-      } catch (err) {
-        await log.error('failed to write device stream/cache', err)
-      }
-      await log.info('new', providerName(res.locals.provider_id), 'vehicle added', device)
-      try {
-        await writeRegisterEvent()
-      } catch (err) {
-        await log.error('writeRegisterEvent failure', err)
-      }
-      res.status(201).send({ result: 'register device success', recorded, device })
-    } catch (err) {
-      if (String(err).includes('duplicate')) {
-        res.status(409).send({
-          error: 'already_registered',
-          error_description: 'A vehicle with this device_id is already registered'
-        })
-      } else if (String(err).includes('db')) {
-        await log.error(providerName(res.locals.provider_id), 'register vehicle failed:', err)
-        res.status(500).send(new ServerError())
-      } else {
-        await log.error(providerName(res.locals.provider_id), 'register vehicle failed:', err)
-        res.status(500).send(new ServerError())
-      }
-    }
-  })
+  app.post(pathsFor('/vehicles'), registerVehicle)
 
   /**
    * Read back a vehicle.
