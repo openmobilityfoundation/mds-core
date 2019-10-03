68c1965c65c7e1c9422804aec8be97cf0248e3cc
mdurling <michael@ellis-and-associates.com>
2019-10-02 19:49:38 -0700

    Minor refactor

--- container-images/env-inject/index.ts
+++ container-images/env-inject/index.ts
@@ -21,12 +21,11 @@ declare const NPM_PACKAGE_GIT_BRANCH: string
 declare const NPM_PACKAGE_GIT_COMMIT: string
 declare const NPM_PACKAGE_BUILD_DATE: string
 
-export function env() {
-  return Object.assign(process.env, {
+export const env = () =>
+  Object.assign(process.env, {
     npm_package_name: NPM_PACKAGE_NAME,
     npm_package_version: NPM_PACKAGE_VERSION,
     npm_package_git_branch: NPM_PACKAGE_GIT_BRANCH,
     npm_package_git_commit: NPM_PACKAGE_GIT_COMMIT,
     npm_package_build_date: NPM_PACKAGE_BUILD_DATE
   })
-}
