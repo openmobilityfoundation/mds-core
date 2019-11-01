58b2839e08b23fbbc89c09fc87b4f4b10553ca5f
Max Johansen <macsj200@gmail.com>
2019-11-01 13:37:54 -0600

    Add perf logging to MDS Daily db/cache calls

--- packages/mds-daily/db-helpers.ts
+++ packages/mds-daily/db-helpers.ts
@@ -9,7 +9,11 @@ import { DbHelperArgs } from './types'
 
 export const getTripCountsSince = async ({ start_time, end_time, provider_info, fail }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getTripCountsPerProviderSince(start_time, end_time)
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getTripCountsPerProviderSince() time elapsed: ${timeElapsed}`)
     await log.info('trips last 24h', rows)
     rows.map(row => {
       const pid = row.provider_id
@@ -23,7 +27,11 @@ export const getTripCountsSince = async ({ start_time, end_time, provider_info, 
 
 export const getTimeSinceLastEvent = async ({ provider_info, fail }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getMostRecentEventByProvider()
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getMostRecentEventByProvider() time elapsed: ${timeElapsed}`)
     await log.info('time since last event', rows)
     rows.map(row => {
       const pid = row.provider_id
@@ -37,7 +45,11 @@ export const getTimeSinceLastEvent = async ({ provider_info, fail }: DbHelperArg
 
 export const getEventCountsPerProviderSince = async ({ start_time, end_time, provider_info, fail }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getEventCountsPerProviderSince(start_time, end_time)
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getEventCountsPerProviderSince() time elapsed: ${timeElapsed}`)
     await log.info('time since last event', rows)
     rows.map(row => {
       const pid = row.provider_id
@@ -59,7 +71,11 @@ export const getTelemetryCountsPerProviderSince = async ({
   fail
 }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getTelemetryCountsPerProviderSince(start_time, end_time)
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getTelemetryCountsPerProviderSince() time elapsed: ${timeElapsed}`)
     await log.info('time since last event', rows)
     rows.map(row => {
       const pid = row.provider_id
@@ -79,7 +95,11 @@ export const getNumVehiclesRegisteredLast24Hours = async ({
   fail
 }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getNumVehiclesRegisteredLast24HoursByProvider(start_time, end_time)
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getNumVehiclesRegisteredLast24HoursByProvider() time elapsed: ${timeElapsed}`)
     await log.info('num vehicles since last 24', rows)
     rows.map(row => {
       const pid = row.provider_id
@@ -93,7 +113,11 @@ export const getNumVehiclesRegisteredLast24Hours = async ({
 
 export const getNumEventsLast24Hours = async ({ start_time, end_time, provider_info, fail }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getNumEventsLast24HoursByProvider(start_time, end_time)
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getNumEventsLast24HoursByProvider() time elapsed: ${timeElapsed}`)
     rows.map(row => {
       const pid = row.provider_id
       provider_info[pid] = provider_info[pid] || {}
@@ -106,7 +130,11 @@ export const getNumEventsLast24Hours = async ({ start_time, end_time, provider_i
 
 export const getConformanceLast24Hours = async ({ start_time, end_time, provider_info, fail }: DbHelperArgs) => {
   try {
+    const start = now()
     const rows = await db.getEventsLast24HoursPerProvider(start_time, end_time)
+    const finish = now()
+    const timeElapsed = finish - start
+    await log.info(`MDS-DAILY db.getEventsLast24HoursPerProvider() time elapsed: ${timeElapsed}`)
     const prev_event: { [key: string]: VehicleEvent } = {}
     await log.info('event', rows)
     rows.map(event => {
