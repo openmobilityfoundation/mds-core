4341c0096729a69665d353dea186ee17b06c2c9a
Michael Durling <michael@ellis-and-associates.com>
2019-12-11 11:49:44 -0500

    Fix debugging for unit tests (#105)

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,6 @@ module.exports = {
   colors: true,
   recursive: true,
   require: ['tsconfig-paths/register', 'source-map-support/register', 'dotenv/config'],
+  spec: 'tests/**/*.ts',
   timeout: 10000
 }
