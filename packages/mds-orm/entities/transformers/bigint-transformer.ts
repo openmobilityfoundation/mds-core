86db5ddbcd1b28e9fdd79277f2d1fc2d8a55e175
drtyh20 <michael@ellis-and-associates.com>
2020-01-21 13:38:37 -0500

    Fix BigintTransformer handling of null values

--- packages/mds-orm/entities/transformers/bigint-transformer.ts
+++ packages/mds-orm/entities/transformers/bigint-transformer.ts
@@ -1,5 +1,7 @@
+const Bigint = (value: string | null): number | null => (value === null ? value : Number(value))
+
 // Use Number for bigint columns so the values get returned as numbers instead of strings
 export const BigintTransformer = {
-  to: (to: number | number[]) => to,
-  from: (from: string | string[]) => (Array.isArray(from) ? from.map(Number) : Number(from))
+  to: (to: number | null | (number | null)[]) => to,
+  from: (from: string | null | (string | null)[]) => (Array.isArray(from) ? from.map(Bigint) : Bigint(from))
 }
