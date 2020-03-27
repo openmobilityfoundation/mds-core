6255fedde9298d0f6889a740ec0c5230edc971c0
Jane Huang <jane.x.huang@gmail.com>
2020-03-27 17:33:52 -0400

    minor fixes

--- packages/mds-policy/types.ts
+++ packages/mds-policy/types.ts
@@ -1,4 +1,14 @@
 import { ApiRequest, ApiResponse } from '@mds-core/mds-api-server'
 
-export type PolicyApiRequest = ApiRequest
-export type PolicyApiResponse = ApiResponse
+export const POLICY_API_SUPPORTED_VERSIONS = ['0.1.0'] as const
+export type POLICY_API_SUPPORTED_VERSION = typeof GEOGRAPHY_API_SUPPORTED_VERSIONS[number]
+export const [POLICY_API_DEFAULT_VERSION] = GEOGRAPHY_API_SUPPORTED_VERSIONS
+
+// Allow adding type definitions for Express Request objects
+export type PolicyApiRequest<P extends Params = ParamsDictionary> = ApiRequest<P>
+
+export interface PolicyApiResponse<TBody extends {}> extends ApiVersionedResponse<GEOGRAPHY_API_SUPPORTED_VERSION, TBody> {
+  locals: ApiVersionedResponseLocals<GEOGRAPHY_API_SUPPORTED_VERSION> & {
+    provider_id: UUID
+  }
+}
