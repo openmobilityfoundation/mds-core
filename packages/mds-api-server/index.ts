f0aff96777ca316870f5638ab2e843a071bd061a
Michael Durling <michael@ellis-and-associates.com>
2020-04-22 15:17:49 -0400

    Allow array of errors in API response (#272)

--- packages/mds-api-server/index.ts
+++ packages/mds-api-server/index.ts
@@ -9,9 +9,10 @@ import { Params, ParamsDictionary } from 'express-serve-static-core'
 
 export type ApiRequest<P extends Params = ParamsDictionary> = express.Request<P>
 
-export type ApiQuery<Q extends string> ={ query:  Partial<{ [P in Q]: string | string[] }> }
+export type ApiQuery<Q extends string> = { query: Partial<{ [P in Q]: string | string[] }> }
 
-export interface ApiResponse<L = unknown, B = unknown> extends express.Response<B | { error: unknown }> {
+export interface ApiResponse<L = unknown, B = unknown>
+  extends express.Response<B | { error: unknown } | { errors: unknown[] }> {
   locals: L
 }
 
