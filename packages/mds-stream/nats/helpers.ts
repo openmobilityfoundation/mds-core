baca413b7f46253e14300bafc33a20e2f274bf03
Neil Goldader <neilgoldader@gmail.com>
2020-05-19 17:27:21 -0400

    Change ts-nats import to properly resolve methods (#338)

--- packages/mds-stream/nats/helpers.ts
+++ packages/mds-stream/nats/helpers.ts
@@ -1,10 +1,10 @@
-import nats from 'ts-nats'
+import { connect, MsgCallback, SubscriptionOptions, Client } from 'ts-nats'
 import logger from '@mds-core/mds-logger'
 import { getEnvVar } from '@mds-core/mds-utils'
 
 const initializeNatsClient = () => {
   const { NATS } = getEnvVar({ NATS: 'localhost' })
-  return nats.connect({
+  return connect({
     url: `nats://${NATS}:4222`,
     reconnect: true,
     waitOnFirstConnect: true,
@@ -14,8 +14,8 @@ const initializeNatsClient = () => {
 
 export const createStreamConsumer = async (
   topic: string,
-  processor: nats.MsgCallback,
-  options: nats.SubscriptionOptions = {}
+  processor: MsgCallback,
+  options: SubscriptionOptions = {}
 ) => {
   const natsClient = await initializeNatsClient()
 
@@ -32,4 +32,4 @@ export const createStreamProducer = async () => {
   return initializeNatsClient()
 }
 
-export const disconnectClient = (consumer: nats.Client) => consumer.close()
+export const disconnectClient = (consumer: Client) => consumer.close()
