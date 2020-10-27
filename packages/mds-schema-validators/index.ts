82bdfd70be6ee9aa430055a050b3f5f51967f9b7
Michael Durling <michael.durling@lacuna.ai>
2020-10-27 10:22:53 -0400

    Update schema validator to support options (#472)

--- packages/mds-schema-validators/index.ts
+++ packages/mds-schema-validators/index.ts
@@ -1,15 +1,15 @@
 import Joi from 'joi'
 import { ValidationError } from '@mds-core/mds-utils'
 
-export const schemaValidator = <T>(schema: Joi.Schema) => ({
-  validate: (value: unknown): T => {
-    const { error } = schema.validate(value)
+export const schemaValidator = <T>(schema: Joi.Schema, options?: Joi.ValidationOptions) => ({
+  validate: (input: unknown): T => {
+    const { error, value } = schema.validate(input, options)
     if (error) {
-      throw new ValidationError(error.message, value)
+      throw new ValidationError(error.message, input)
     }
     return value as T
   },
-  isValid: (value: unknown): value is T => !schema.validate(value).error
+  isValid: (input: unknown): input is T => !schema.validate(input, options).error
 })
 
 export * from './validators'
