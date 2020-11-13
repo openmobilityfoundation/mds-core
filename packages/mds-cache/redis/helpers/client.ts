5e3868a89ac63961cf7c7485b313f4049b33fe89
Mark Maxham <max@ellis-and-associates.com>
2020-11-13 13:25:34 -0800

    Fix/redis istio connect bug (#492)
    * make sure that initial connection failure doesn't totally hork anything that uses the cache
    because istio can fail to start the network stack ahead of bringing the service up
    
    * Formatting cleanup
    
    Co-authored-by: Neil Goldader <neilgoldader@gmail.com>

--- packages/mds-cache/redis/helpers/client.ts
+++ packages/mds-cache/redis/helpers/client.ts
@@ -1,5 +1,6 @@
 import Redis from 'ioredis'
 import { cleanEnv, host, num } from 'envalid'
+import logger from '@mds-core/mds-logger'
 
 const { REDIS_PORT, REDIS_HOST, REDIS_PASS } = cleanEnv(process.env, {
   REDIS_PORT: num({ default: 6379 }),
@@ -7,9 +8,27 @@ const { REDIS_PORT, REDIS_HOST, REDIS_PASS } = cleanEnv(process.env, {
 })
 
 export const initClient = async () => {
-  const client = new Redis({ lazyConnect: true, port: REDIS_PORT, host: REDIS_HOST, password: REDIS_PASS })
+  const client = new Redis({
+    lazyConnect: true,
+    maxRetriesPerRequest: 1000, // 20 is default, but that may not be long enough (thanks istio)
+    port: REDIS_PORT,
+    host: REDIS_HOST,
+    password: REDIS_PASS
+  })
 
-  await client.connect()
+  client.on('connect', () => {
+    logger.warn('Redis connection established')
+  })
+
+  try {
+    /*
+      The ioredis client will attempt to retry establishing a connection, however the `.connect` method will throw upon the first failure...
+      This wrapper attempts to connect; and if fails initially, catches it. IORedis will continue retrying in the background, so no worries.
+    */
+    await client.connect()
+  } catch (err) {
+    logger.error('Initial redis connection failure (connection will be retried):', err)
+  }
 
   return client
 }
