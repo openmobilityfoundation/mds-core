135dfb6a5ab901e2a7e8c590ce666d679390cda9
Carter Lynch <carter.lynch@lacuna.ai>
2021-08-03 11:51:17 -0400

    Add missing migration name property to AddEventsRowIdColumn1627427307591

---
+++ .changeset/orange-garlics-compare.md
@@ -0,0 +1,5 @@
+---
+"@mds-core/mds-ingest-service": minor
+---
+
+Add missing migration name property to migration AddEventsRowIdColumn1627427307591
--- packages/mds-ingest-service/repository/migrations/1627427307591-AddEventsRowIdColumn.ts
+++ packages/mds-ingest-service/repository/migrations/1627427307591-AddEventsRowIdColumn.ts
@@ -1,6 +1,8 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class AddEventsRowIdColumn1627427307591 implements MigrationInterface {
+  name = 'AddEventsRowIdColumn1627427307591'
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "event_annotations" ADD "events_row_id" bigint DEFAULT NULL`)
     await queryRunner.query(
