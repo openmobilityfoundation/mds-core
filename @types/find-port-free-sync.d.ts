e6c46989e86966a40d231fcde74b3ffaec9660da
Neil Goldader <neilgoldader@gmail.com>
2021-08-17 17:24:52 -0400

    Fix randport typedef & usage (#703)

--- @types/find-port-free-sync.d.ts
+++ @types/find-port-free-sync.d.ts
@@ -1,5 +1,7 @@
 declare module 'find-port-free-sync' {
-  function findPortFreeSync(start: number = 1, end: number = 65534, num: number = 1, ip: string = '127.0.0.1', port: number | null = null): number
+  type findPortOptions = { start?: number = 1, end?: number = 65534, num?: number = 1, ip?: string = '127.0.0.1', port?: number | null = null }
+
+  function findPortFreeSync(options: findPortOptions): number
 
   export default findPortFreeSync
 }
